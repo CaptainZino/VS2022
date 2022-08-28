c75608a3864b96341c8e9e28c4150dc740ce8478
DecinoXz <zhuliangxiong@163.com>
2022-08-28 11:27:33 +0800

    code for practice

---
+++ DebugCode/DebugCode/test.c
@@ -0,0 +1,71 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <stdlib.h>
+
+//int main()
+//{
+//	int* p = malloc(10);
+//	//*p = 10;
+//	int* parr = p;
+//	//p++;
+//	free(parr);
+//	*p = 5;
+//	//*p = 5;
+//	/*printf("%d\n",*p);
+//	free(p);*/
+//	return 0;
+//}
+
+//int main()
+//{
+//	/*int* p = malloc(10);
+//	printf("%d\n",*p);
+//	int* parr = calloc(4, 10);
+//	printf("%d\n", *parr);*/
+//	//printf("%d\n");
+//	int i = 0;
+//	int j = 0;
+//	char arr1[] = "abcdef";
+//	char arr2[] = "123456789";
+//	char temp;
+//	printf(arr1);
+//	printf("\n");
+//	printf(arr2);
+//	printf("\n");
+//	for (i = 0, j = 0; sizeof(arr1)>sizeof(arr2)?arr2[j] != '\0':arr1[i] !='\0'; i++, j++)
+//		temp = arr1[i], arr1[i] = arr2[j], arr2[j] = temp;  //∂∫∫≈±Ì¥Ô Ω
+//	printf(arr1);
+//	printf("\n");
+//	printf(arr2);
+//	printf("\n");
+//	return 0;
+//}
+
+//#pragma pack(1)
+//struct str
+//{
+//	char a[5];
+//	int b;
+//	char c;
+//};
+//#pragma pack()
+//struct str1
+//{
+//	char a[5];
+//	int b;
+//	char c;
+//};
+//int main()
+//{	
+//	struct str stra;
+//	struct str1 stra1;
+//	printf("%d\n",sizeof(stra));
+//	printf("%d\n", sizeof(stra1));
+//	return 0;
+//}
+
+int main()
+{
+	//bool a;
+	return 0;
+}
